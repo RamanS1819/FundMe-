6d4e2af642c7d47259851ad609ca27f2b2aa8b1b
RamanS1819 <ramanspatel1819@gmail.com>
2025-01-08 21:44:24 +0530

    refactoring contract

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     // us -> FundMeTest -> FundMe
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public view {
@@ -17,6 +20,22 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsDeployer() public view {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
+
+    // What can we do to work with addresses outside our system?
+    // 1. Unit
+    //    - Testing a specific part of our code
+    // 2. Integration
+    //    - Testing how our code works with other parts of code
+    // 3. Forked
+    //    - Testing how our code works with the real world (testing our code on a simulated real environment)
+    // 4. Staging
+    //   - Testing our code in a real environment that is not prod
+
+      function testPriceFeedVersionIsAccurate() public view {
+            uint256 version = fundMe.getVersion();
+            assertEq(version, 6);
+      }
+
 }
