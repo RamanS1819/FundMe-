d6ed41891bb30b86e4bccb033757e33cc617c566
RamanS1819 <ramanspatel1819@gmail.com>
2025-01-10 23:57:15 +0530

    formatting

--- script/Interaction.s.sol
+++ script/Interaction.s.sol
@@ -9,39 +9,32 @@ import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundFundMe is Script {
-      uint256 SEND_VALUE = 0.01 ether;
+    uint256 SEND_VALUE = 0.01 ether;
 
-      function fundFundMe(address mostRecentlyDeployed) public {
-            vm.startBroadcast();
-            FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
-            console.log("Funded FundMe with %s ETH", SEND_VALUE);
-            vm.stopBroadcast();
-      }
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        console.log("Funded FundMe with %s ETH", SEND_VALUE);
+        vm.stopBroadcast();
+    }
 
-      function run() external {
-            address mostRecentDeployed = DevOpsTools.get_most_recent_deployment(
-                  "FundMe",
-                  block.chainid
-            );
-            fundFundMe(mostRecentDeployed);
-      }
+    function run() external {
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        fundFundMe(mostRecentDeployed);
+    }
 }
 
 contract WithdrawFundMe is Script {
-      function withdrawFundMe(address mostRecentlyDeployed) public {
-            vm.startBroadcast();
-            FundMe(payable(mostRecentlyDeployed)).withdraw();
-            vm.stopBroadcast();
-      }
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+    }
 
-      function run() external {
-            address mostRecentDeployed = DevOpsTools.get_most_recent_deployment(
-                  "FundMe",
-                  block.chainid
-            );
-            // vm.startBroadcast();
-            withdrawFundMe(mostRecentDeployed);
-            // vm.stopBroadcast();
-
-      }
+    function run() external {
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        // vm.startBroadcast();
+        withdrawFundMe(mostRecentDeployed);
+        // vm.stopBroadcast();
+    }
 }
--- test/integration/InteractionsTest.t.sol
+++ test/integration/InteractionsTest.t.sol
@@ -7,28 +7,27 @@ import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 import {FundFundMe, WithdrawFundMe} from "../../script/Interaction.s.sol";
 
 contract InteractionsTest is Test {
+    FundMe fundMe;
 
-      FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether; // 100000000000000000 wei
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
 
-      address USER = makeAddr("user");
-      uint256 constant SEND_VALUE = 0.1 ether; // 100000000000000000 wei
-      uint256 constant STARTING_BALANCE = 10 ether;
-      uint256 constant GAS_PRICE = 1;
-
-      function setUp() external {
+    function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
         vm.deal(USER, STARTING_BALANCE);
-      }
+    }
 
-      function testUserCanFundInteractions() public {
-            FundFundMe fundFundMe = new FundFundMe();
-            fundFundMe.fundFundMe(address(fundMe));
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
 
-            WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
-            withdrawFundMe.withdrawFundMe(address(fundMe));
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
 
-            assert(address(fundMe).balance == 0);
-      }
+        assert(address(fundMe).balance == 0);
+    }
 }
